749c60a2f2157262ac3a85145e924969197834c9
zigamacele <19758116+zigamacele@users.noreply.github.com>
2023-07-29 08:51:03 +0200

    fix: current table

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,33 @@
 import { getGachaGames, inputMonthlyStatistics } from './helpers/supabase'
 import { getMonthlyStatistics } from './logic/pupppet'
 import { Gacha } from './types/global'
+import { getCurrentTable } from './utils/timeDate'
 
 const startGatheringInformation = async () => {
   const data = await getGachaGames()
-  const updateEveryXMinutes = 2
+  const updateEveryXMinutes = 1
 
   const gatheredInformation: Gacha[] = []
 
   if (data) {
+    console.log('TABLE:' + getCurrentTable())
+    console.log('STARTING TO GATHER INFORMATION')
     for (const [index, game] of data.entries()) {
       setTimeout(
         async () => {
           const monthlyStats = await getMonthlyStatistics(game.name, game.id)
           gatheredInformation.push(monthlyStats)
 
-          if (index + 1 === data.length)
+          if (index + 1 === data.length) {
             await inputMonthlyStatistics(gatheredInformation)
+            console.log('FINISHED GATHERING INFORMATION')
+          }
         },
         updateEveryXMinutes * 60 * 1000 * index,
       )
     }
+  } else {
+    console.log('SOMETHING WENT WRONG WITH THE DATABASE')
   }
 }
 
--- src/utils/timeDate.ts
+++ src/utils/timeDate.ts
@@ -1,5 +1,9 @@
 import dayjs from 'dayjs'
 
 export const getCurrentTable = () => {
-  return `${dayjs().month() + 1}-${dayjs().year()}`
+  if (!dayjs().month()) {
+    return `11-${dayjs().year() - 1}`
+  }
+
+  return `${dayjs().month()}-${dayjs().year()}`
 }
