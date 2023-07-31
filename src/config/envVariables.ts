80072adc1dbee4af38cc91f678d0c3c41e6a85af
zigamacele <19758116+zigamacele@users.noreply.github.com>
2023-07-31 19:09:08 +0200

    chore: added games table name to env

--- src/config/envVariables.ts
+++ src/config/envVariables.ts
@@ -3,7 +3,11 @@ dotenv.config()
 
 const config = {
   website: { URL: getConfig('WEBSITE_URL') },
-  database: { HOST: getConfig('DB_HOST'), SECRET: getConfig('DB_SECRET') },
+  database: {
+    HOST: getConfig('DB_HOST'),
+    SECRET: getConfig('DB_SECRET'),
+    GAMES_TABLE: getConfig('DB_GAMES_TABLE'),
+  },
 }
 
 function getConfig(envKey: string): string {
--- src/helpers/supabase.ts
+++ src/helpers/supabase.ts
@@ -1,9 +1,12 @@
+import config from '../config/envVariables'
 import supabase from '../config/supabaseClient'
 import { Gacha } from '../types/global'
 import { getCurrentTable } from '../utils/timeDate'
 
 export const getGachaGames = async () => {
-  const { data, error } = await supabase.from('games').select('*')
+  const { data, error } = await supabase
+    .from(config.database.GAMES_TABLE)
+    .select('*')
 
   if (error) console.log(error)
 
