c80f39c01931505b71c796e772fe71fbd0f46fac
zigamacele <19758116+zigamacele@users.noreply.github.com>
2023-12-18 16:35:43 +0100

    refactor: moved prompQuestions to constants

--- src/logic/inquirer.ts
+++ src/logic/inquirer.ts
@@ -1,67 +1,10 @@
 import inquirer from 'inquirer'
 
+import { promptQuestions } from '../constants/inquirer'
 import { Answers } from '../types/inquirer'
-import { greenTextParenthesis } from '../utils/format'
-import { getCurrentTable } from '../utils/timeDate'
 
-const timeoutChoices = [
-  { name: '0.25 min', value: 0.25 },
-  { name: '0.5 min', value: 0.5 },
-  { name: '1 min', value: 1 },
-]
-
-const modeChoices = [
-  { name: 'Full', value: 'full' },
-  { name: 'Partial', value: 'partial' },
-]
-
-const promptQuestions = [
-  {
-    name: 'mode',
-    type: 'list',
-    message: 'In what mode do you want to run Tayu:',
-    choices: modeChoices,
-  },
-  {
-    name: 'partial',
-    type: 'input',
-    message: 'Enter array of game IDs to run:',
-    when: (answers: Answers) => answers.mode === 'partial',
-  },
-  {
-    name: 'defaultTimeout',
-    type: 'confirm',
-    message: `Default timeout? ${greenTextParenthesis(
-      timeoutChoices[1]?.name,
-    )}`,
-  },
-  {
-    name: 'timeout',
-    type: 'list',
-    message: 'Timeout between games:',
-    choices: timeoutChoices,
-    when: (answers: Answers) => !answers.defaultTimeout,
-  },
-  {
-    name: 'currentTable',
-    type: 'confirm',
-    message: `Current months table? ${greenTextParenthesis(getCurrentTable())}`,
-  },
-  {
-    name: 'table',
-    type: 'input',
-    message: 'Enter table name:',
-    when: (answers: Answers) => !answers.currentTable,
-  },
-  {
-    name: 'perms',
-    type: 'confirm',
-    message: 'Test database permissions?',
-  },
-]
-
-export const getSessionConfig = () => {
-  return inquirer
+export const getSessionConfig = async () => {
+  return await inquirer
     .prompt(promptQuestions)
     .then((answers: Answers) => {
       return answers
